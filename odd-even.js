d33edfe87d06167c55101ca65c6842f6acd0811f
Showrav108 <shahidcse23@gmail.com>
2025-02-09 20:14:20 +0600

    odd-even code completed

---
+++ odd-even.js
@@ -0,0 +1,38 @@
+// 1 : Find all the odd numbers from 61 to 100.
+
+
+// for loop
+for(num = 61; num <= 100; num++){
+    if(num % 2 === 1){
+        console.log(num);
+    }
+}
+
+// while loop
+// let num = 61;
+// while(num <= 100){ 
+//     if(num % 2 === 1){
+//         console.log(num);    
+//     }
+//     num++;
+// }
+
+
+
+// 2 : Find all the even numbers from 78 to 98.
+
+// for loop
+// for(num = 78; num <= 98; num++){
+//     if(num % 2 === 0){
+//        console.log(num);
+//     }
+// }
+
+// while loop
+let num = 78;
+while(num <= 98){ 
+    if(num % 2 === 0){
+        console.log(num);    
+    }
+    num++;
+}
