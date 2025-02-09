0f5f4f42da77084e3280ef1c4933d050e157498e
Showrav108 <shahidcse23@gmail.com>
2025-02-10 00:59:12 +0600

    sum numbers 0f odd even completed

---
+++ sum-numbers.js
@@ -0,0 +1,50 @@
+// 1 : Display sum of all the odd numbers from 81 to 131.
+
+// for loop
+// let sum = 0;
+// for(num = 81; num <= 131; num++){
+//     if(num % 2 ===1){
+//         console.log(num);
+//         sum = sum + num;     
+//     }
+// }
+// console.log('sum of numbers from 81 to 131 is', sum);
+
+// while loop
+// let num = 81;
+// let sum = 0;
+// while(num <= 131){
+//     if(num % 2 ===1){     
+//         sum = sum + num;
+//         console.log(num);
+//     }
+//     num++;
+// }
+// console.log('sum of numbers from 81 to 131 is', sum);
+
+                
+// 2 : Display sum of all the even numbers from 206 to 311.
+
+// for loop
+// let sum = 0;
+// for(num = 206; num <= 311; num++){
+//     if(num % 2 ===0){
+//         console.log(num);
+//         sum = sum + num;     
+//     }
+// }
+// console.log('sum of numbers from 206 to 311 is', sum);
+
+
+
+// while loop
+let num = 206;
+let sum = 0;
+while(num <= 311){
+    if(num % 2 ===0){     
+        sum = sum + num;
+        console.log(num);
+    }
+    num++;
+}
+console.log('sum of numbers from 206 to 311 is', sum);
