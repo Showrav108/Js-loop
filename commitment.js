304225a5f90d14a1c5d850fe53d63ad47cad8f06
Showrav108 <shahidcse23@gmail.com>
2025-02-09 19:47:35 +0600

    commitment code completed

---
+++ commitment.js
@@ -0,0 +1,22 @@
+/***
+
+"I will invest at least 6 hrs every single day for next 60 days!" 
+this message 60 times. So display this.
+
+ */
+
+
+// for loop
+
+for(i = 1; i <= 60; i++){
+    console.log("I will invest at least 6 hrs every single day for next 60 days!");
+}
+
+
+// while loop
+
+let n = 1;
+while(n <= 60){
+    n++;
+    console.log("I will invest at least 6 hrs every single day for next 60 days!");
+}
